9054fe3632ac888577b2379a411862b123e8a472
Tathagata8 <74824304+Tathagata8@users.noreply.github.com>
2021-01-06 23:17:21 +0800

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -7,11 +7,12 @@ class Ball{
             friction:0.5,
             density:1.2
         }
+        this.image = loadImage("paper.png")
         this.x=x;
         this.y=y;
         this.r=r;
         this.body=Bodies.circle(this.x,this.y,this.r/2,options);
-        this.image = loadImage("paper/paper.png")
+       
         World.add(myworld,this.body);
     }
     display(){
