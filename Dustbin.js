8be8bdcf6db2c3f869385b503602a740a3699a22
Tathagata8 <74824304+Tathagata8@users.noreply.github.com>
2021-01-06 23:16:07 +0800

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -11,19 +11,21 @@ class Dustbin{
       this.dustbinHeight = 100;
       this.wallThickness = 20;
       this.angle = 0;
-
-      this.leftbody = Bodies.rectangle(this.x -this.dustbinWidth/2, this.y - this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, options);
-      Matter.Body.setAngle(this.leftbody,this.angle);
-      World.add(myworld,this.leftbody)
-      
-      this.bottombody = Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, options);
+   this.image = loadImage("dustbingreen/dustbingreen.png")
+        this.bottombody = Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, options);
        World.add(myworld,this.bottombody);
+    
+        this.leftbody = Bodies.rectangle(this.x -this.dustbinWidth/2, this.y - this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, options);
+      Matter.Body.setAngle(this.leftbody,this.angle);
+      World.add(myworld,this.leftbody);
+      
+    ;
       
        this.rightbody = Bodies.rectangle(this.x + this.dustbinWidth/2, this.y - this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, options);
        Matter.Body.setAngle(this.rightbody, -1*this.angle);
        World.add(myworld,this.rightbody);
       
-       this.image = loadImage("dustbingreen/dustbingreen.png")
+      
       
     }
 
@@ -40,7 +42,7 @@ class Dustbin{
            fill("white");
            angleMode(RADIANS);
            rotate(this.angle);
-           rect(0,0,this.wallThickness,this.dustbinHeight);
+          //rect(0,0,this.wallThickness,this.dustbinHeight);
            pop()
 
            push() 
@@ -50,18 +52,18 @@ class Dustbin{
            fill("white");
            angleMode(RADIANS);
            rotate(-1*this.angle);
-           rect(0,0,this.wallThickness,this.dustbinHeight);
+           //rect(0,0,this.wallThickness,this.dustbinHeight);
            pop();
 
-           push() 
-           translate(posbottom.x, posbottom.y);
-           imageMode(CENTER)
-           image(this.image,0, 0, this.width, this.height);   
+            push() 
+           translate(posbottom.x, posbottom.y+10);
+          
            fill("white");
            angleMode(RADIANS);
-           rotate(this.angle);
-           rect(0,0,this.dustbinWidth,this.wallThickness);
+           imageMode(CENTER);
+           image(this.image,0,-this.dustbinHeight/2,this.dustbinWidth,this.wallThickness);
            pop();
+
 
         }
 }
